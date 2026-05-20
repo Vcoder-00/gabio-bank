148ccddac942a538f9867fd0a1087d7b019056b5
Vitor Lopes <vcoder00@gmail.com>
2026-05-20 15:19:09 -0300

    fix(infra): corrigir dados e contas fake

--- src/infra/contas.ts
+++ src/infra/contas.ts
@@ -1,21 +1,4 @@
-export class Conta {
-
-  constructor(
-    private id: string,
-    private saldo: number
-  ) {}
-
-  public getId(): string {
-    return this.id;
-  }
-
-  public getSaldo(): number {
-    return this.saldo;
-  }
-
-  public alteraSaldo(novo:number){
-    this.saldo = novo;
-  }
-
-
-  }
+export type Conta = {
+  id: string;
+  saldo: number;
+};
